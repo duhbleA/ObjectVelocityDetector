ead5ecf169f7fb8718117397c00dccbb22c2c059
AustinA <ada0022@uah.edu>
2018-10-23 09:48:49 -0500

    Add warning message about program requirements for its location.

--- ObjectVelocityDetector/src/main.cpp
+++ ObjectVelocityDetector/src/main.cpp
@@ -4,47 +4,44 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include "../include/conversion.h"
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     // Initialize the Python Interpreter and add all of the local modules
     Py_Initialize();
-    PyObject *sysPath = PySys_GetObject((char*)"path");
+    PyObject *sysPath = PySys_GetObject((char *) "path");
 
-    PyList_Append(sysPath, PyUnicode_FromString((char *)"."));
+    PyList_Append(sysPath, PyUnicode_FromString((char *) "."));
 
 
     // Create the main module (main.py)
-    PyObject* moduleString = PyUnicode_FromString((char*) "main");
-    PyObject* mainModule = PyImport_Import(moduleString);
+    PyObject *moduleString = PyUnicode_FromString((char *) "main");
+    PyObject *mainModule = PyImport_Import(moduleString);
 
 
-    if (mainModule != nullptr)
-    {
+    if (mainModule != nullptr) {
         // Create an object that can invoke the start() function defined in main.py
-        PyObject* startFunction = PyObject_GetAttrString(mainModule ,(char*) "start");
+        PyObject *startFunction = PyObject_GetAttrString(mainModule, (char *) "start");
 
-        if (startFunction && PyCallable_Check(startFunction))
-        {
+        if (startFunction && PyCallable_Check(startFunction)) {
             // Create a default blank argument and invoke start() defined in main.py
-            PyObject* startArg = PyTuple_New(0);
+            PyObject *startArg = PyTuple_New(0);
             PyObject_CallObject(startFunction, startArg);
             Py_DECREF(startArg);
             Py_DECREF(startFunction);
 
             // Create an object that can invoke execute_session("1") defined in main.py
-            PyObject* executeSession1 = PyObject_GetAttrString(mainModule ,(char*) "execute_session");
-            PyObject* executeArg1 = PyTuple_Pack(1, PyUnicode_FromString((char*)"0"));
+            PyObject *executeSession1 = PyObject_GetAttrString(mainModule, (char *) "execute_session");
+            PyObject *executeArg1 = PyTuple_Pack(1, PyUnicode_FromString((char *) "0"));
 
             // Create an object that can invoke execute_session("2") defined in main.py
-            PyObject* executeSession2 = PyObject_GetAttrString(mainModule ,(char*) "execute_session");
-            PyObject* executeArg2 = PyTuple_Pack(1, PyUnicode_FromString((char*)"1"));
+            PyObject *executeSession2 = PyObject_GetAttrString(mainModule, (char *) "execute_session");
+            PyObject *executeArg2 = PyTuple_Pack(1, PyUnicode_FromString((char *) "1"));
 
             // Create an object that can invoke window_terminated_requested() defined in main.py
-            PyObject* exitWindowTest = PyObject_GetAttrString(mainModule, (char*) "window_terminated_requested");
-            PyObject* exitWindowArg = PyTuple_New(0);
+            PyObject *exitWindowTest = PyObject_GetAttrString(mainModule, (char *) "window_terminated_requested");
+            PyObject *exitWindowArg = PyTuple_New(0);
 
             // The function result of window_terminated_requested
-            PyObject* windowCheck;
+            PyObject *windowCheck;
 
             // Converts ndarray of numpy values to a cv::Mat
             NDArrayConverter cvt;
@@ -53,15 +50,13 @@ int main(int argc, char *argv[])
             // Reuse reference
             PyObject *ex1ret;
             PyObject *ex2ret;
-            while (true)
-            {
+            while (true) {
                 if (executeSession1 && PyCallable_Check(executeSession1)
-                     && executeSession2 && PyCallable_Check(executeSession2)) {
+                    && executeSession2 && PyCallable_Check(executeSession2)) {
 
                     // Call execute_session, and convert its result to a cv::Mat
                     ex1ret = PyObject_CallObject(executeSession1, executeArg1);
-                    if (ex1ret != nullptr)
-                    {
+                    if (ex1ret != nullptr) {
                         matImage1 = cvt.toMat(ex1ret);
 
                         // Display the image
@@ -72,8 +67,7 @@ int main(int argc, char *argv[])
 
                     // Call execute_session, and convert its result to a cv::Mat
                     ex2ret = PyObject_CallObject(executeSession2, executeArg2);
-                    if (ex2ret != nullptr)
-                    {
+                    if (ex2ret != nullptr) {
                         matImage2 = cvt.toMat(ex2ret);
                         // Display the image
                         cv::namedWindow("Display window 2", cv::WINDOW_AUTOSIZE);
@@ -81,10 +75,9 @@ int main(int argc, char *argv[])
                     }
 
                     windowCheck = PyObject_CallObject(exitWindowTest, exitWindowArg);
-                   
+
                     // Terminate the loop if q is pressed, but first clean and dereference
-                    if (PyObject_IsTrue(windowCheck) || ((cv::waitKey(1) & 0xFF) == 113))
-                    {
+                    if (PyObject_IsTrue(windowCheck) || ((cv::waitKey(1) & 0xFF) == 113)) {
                         cv::destroyAllWindows();
                         Py_DECREF(exitWindowTest);
                         Py_DECREF(exitWindowArg);
@@ -99,14 +92,17 @@ int main(int argc, char *argv[])
             }
 
             // Invoke the stop() method defined in main.py
-            PyObject* stop = PyObject_GetAttrString(mainModule, (char*) "window_terminated_requested");
-            PyObject* stopArg = PyTuple_New(0);
+            PyObject *stop = PyObject_GetAttrString(mainModule, (char *) "window_terminated_requested");
+            PyObject *stopArg = PyTuple_New(0);
             PyObject_CallObject(stop, stopArg);
 
             Py_DECREF(stop);
             Py_DECREF(stopArg);
 
         }
+    } else {
+        std::cout << "main.py could not be found. Be sure to run the program from the same directory as main.py"
+                  << std::endl;
     }
     // Terminate the python runtime
     Py_Finalize();
